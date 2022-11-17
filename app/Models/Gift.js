9a3303700d98f99934da8e9e2bcca20b1ae0eaea
Savannah Temple <savtemple@gmail.com>
2022-11-17 13:31:38 -0700

    smol

--- app/Models/Gift.js
+++ app/Models/Gift.js
@@ -1,7 +1,7 @@
 export class Gift {
   constructor(data) {
     this.id = data.id;
-    this.tag = data.tag;
+    this.tag = data.tag || "Write a damn tag";
     this.url =
       data.url ||
       "https://images.unsplash.com/photo-1599508704512-2f19efd1e35f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cXVlc3Rpb24lMjBtYXJrfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60";
