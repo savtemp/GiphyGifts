1bb977f236369719255e8e7c9b51f2dd441f2c25
Savannah Temple <savtemple@gmail.com>
2022-11-17 20:15:39 -0700

    giphy api set up

--- app/Services/AxiosService.js
+++ app/Services/AxiosService.js
@@ -2,3 +2,8 @@ export const api = new axios.create({
   baseURL: "https://bcw-sandbox.herokuapp.com/api/",
   timeout: 8000,
 });
+
+export const giphyApi = new axios.create({
+  baseURL: "http://api.giphy.com/v1/gifs",
+  timeout: 8000,
+});
